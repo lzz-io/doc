e81381b758316d5b9547d0aad237a74acbcabaa3
lzz.io <1219331697@qq.com>
2019-09-19 00:17:53 +0800

    shadowsocks

--- docker/shadowsocks/build.sh
+++ docker/shadowsocks/build.sh
@@ -7,7 +7,11 @@ docker images -a
 docker login -u 15590988@qq.com -p q1w2e3r4 registry-vpc.cn-hongkong.aliyuncs.com
 docker push registry-vpc.cn-hongkong.aliyuncs.com/lzz-io/shadowsocks
 
-
+# # 目前SS端最推荐的加密以及obfs混淆如下：
+# # 推荐加密方式：aes-256-gcm 、chacha20-ietf-poly1305、aes-128-gcm、aes-192-gcm （排名分先后）
+# #
+# # SS最佳推荐加密方式：aes-256-gcm，最佳obfs：http 
+# # 目前手机端、第三方路由固件多数已支持，建议使用以上推荐，PC客户端有待更新，相信不会等太久。
 
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo \
@@ -23,8 +27,8 @@ systemctl restart docker
 # 阿里云
 docker login -u 15590988@qq.com -p q1w2e3r4 registry-vpc.cn-hongkong.aliyuncs.com
 
-docker run -d -p 3333:8388 -p 3333:8388/udp \
-    -e PASSWORD=gfwsb -e METHOD=rc4-md5 -e TZ=CST-8 \
+docker run -d -p 8080:8388 -p 8080:8388/udp \
+    -e PASSWORD=gfwsb -e METHOD=aes-256-cfb -e TZ=CST-8 \
     --name=shadowsocks --restart=always \
     registry-vpc.cn-hongkong.aliyuncs.com/lzz-io/shadowsocks
 
@@ -32,7 +36,7 @@ docker run -d -p 3333:8388 -p 3333:8388/udp \
 # 腾讯云
 docker login -u 15590988@qq.com -p q1w2e3r4 registry.cn-hongkong.aliyuncs.com
 
-docker run -d -p 3333:8388 -p 3333:8388/udp \
+docker run -d -p 8080:8388 -p 8080:8388/udp \
     -e PASSWORD=gfwsb -e METHOD=rc4-md5 -e TZ=CST-8 \
     --name=shadowsocks --restart=always \
     registry.cn-hongkong.aliyuncs.com/lzz-io/shadowsocks
