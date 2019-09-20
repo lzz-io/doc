ab46b026dd1f56bebe7e0b1302509ad10ae7a901
lzz.io <1219331697@qq.com>
2019-09-20 19:35:55 +0800

    使用 shadowsocks/shadowsocks-libev 镜像

--- docker/shadowsocks/build.sh
+++ docker/shadowsocks/build.sh
@@ -13,6 +13,8 @@ docker push registry-vpc.cn-hongkong.aliyuncs.com/lzz-io/shadowsocks
 # # SS最佳推荐加密方式：aes-256-gcm，最佳obfs：http 
 # # 目前手机端、第三方路由固件多数已支持，建议使用以上推荐，PC客户端有待更新，相信不会等太久。
 
+# # iso目前只支持 aes-256-cfb
+
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
@@ -24,20 +26,34 @@ systemctl stop firewalld
 systemctl enable docker
 systemctl restart docker
 
-# 阿里云
-docker login -u 15590988@qq.com -p q1w2e3r4 registry-vpc.cn-hongkong.aliyuncs.com
 
+# docker pull shadowsocks/shadowsocks-libev
+# docker run -d -p 8888:8388 -p 8888:8388/udp \
+        # -e PASSWORD=gfwsb  -e METHOD=aes-256-cfb -e TZ=CST-8 \
+        # --name=shadowsocks --restart=always \
+        # shadowsocks/shadowsocks-libev
+
+docker pull shadowsocks/shadowsocks-libev
 docker run -d -p 8888:8388 -p 8888:8388/udp \
-    -e PASSWORD=gfwsb -e METHOD=aes-256-gcm -e TZ=CST-8 \
-    --name=shadowsocks --restart=always \
-    registry-vpc.cn-hongkong.aliyuncs.com/lzz-io/shadowsocks
+        -e PASSWORD=gfwsb  -e METHOD=aes-256-gcm -e TZ=CST-8 \
+        --name=shadowsocks --restart=always \
+        shadowsocks/shadowsocks-libev
+
+
+# 阿里云
+# docker login -u 15590988@qq.com -p q1w2e3r4 registry-vpc.cn-hongkong.aliyuncs.com
+
+# docker run -d -p 8888:8388 -p 8888:8388/udp \
+    # -e PASSWORD=gfwsb -e METHOD=aes-256-gcm -e TZ=CST-8 \
+    # --name=shadowsocks --restart=always \
+    # registry-vpc.cn-hongkong.aliyuncs.com/lzz-io/shadowsocks
 
 
 # 腾讯云
-docker login -u 15590988@qq.com -p q1w2e3r4 registry.cn-hongkong.aliyuncs.com
+# docker login -u 15590988@qq.com -p q1w2e3r4 registry.cn-hongkong.aliyuncs.com
 
-docker run -d -p 8888:8388 -p 8888:8388/udp \
-    -e PASSWORD=gfwsb -e METHOD=aes-256-gcm -e TZ=CST-8 \
-    --name=shadowsocks --restart=always \
-    registry.cn-hongkong.aliyuncs.com/lzz-io/shadowsocks
+# docker run -d -p 8888:8388 -p 8888:8388/udp \
+    # -e PASSWORD=gfwsb -e METHOD=aes-256-gcm -e TZ=CST-8 \
+    # --name=shadowsocks --restart=always \
+    # registry.cn-hongkong.aliyuncs.com/lzz-io/shadowsocks
 
