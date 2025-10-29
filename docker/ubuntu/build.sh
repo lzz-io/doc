42f4fd55f4cde20d4d334e03fc71b1828258c432
q1219331697 <1219331697@qq.com>
2025-10-29 12:33:08 +0800

    docker ubuntu:systemd

--- docker/ubuntu/build.sh
+++ docker/ubuntu/build.sh
@@ -1,15 +1,5 @@
-docker build -t registry-vpc.cn-hongkong.aliyuncs.com/io-lzz/ubuntu .
+#!/bin/bash
+
+docker build -t ubuntu:systemd .
 
 docker images -a
-
-#docker tag registry-vpc.cn-hongkong.aliyuncs.com/io-lzz/ubuntu registry-vpc.cn-hongkong.aliyuncs.com/io-lzz/ubuntu:1
-
-#docker push registry-vpc.cn-hongkong.aliyuncs.com/io-lzz/ubuntu
-#docker push registry-vpc.cn-hongkong.aliyuncs.com/io-lzz/ubuntu:1
-
-## for docker-compose
-#docker-compose up
-docker-compose run app bash
-
-## for docker stack
-#docker stack deploy -c docker-compose.yml ubuntu
